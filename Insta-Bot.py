6592914ca692b1ace02eafcfa7f33c4fca0f7f1d
bowloforanges <george.soriano.plus@gmail.com>
2020-02-05 14:24:04 -0600

    Project Scaffolding

---
+++ Insta-Bot.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+
+class InstaBot():
+
+    def __init__(self):
+
+        self.driver = webdriver.Chrome()
+
+    def login(self):
+
+        
+
+#https://www.instagram.com/explore/tags/fineartphotography/
